df2cee084e70018636766c38e7e3a28c40f82ea6
Arjuna <20848863+SoftwareDevOps@users.noreply.github.com>
2023-10-22 19:16:06 -0700

    Added Javascript files

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,30 @@
 var topics = ["HTML", "CSS", "Git", "JavaScript"];
+var randomTopic = topics[Math.floor(Math.random()*topics.length)]
 // console.log(topics);
 
-
-// var topic = "HTML";
-
-// if (topic === 'HTML') {
-//   console.log("Let's study HTML!");
-// } else if (topic === 'CSS') {
-//   console.log("Let's study CSS!");
-// } else if (topic === 'Git') {
-//   console.log("Let's study Git!");
-// } else if (topic === 'JavaScript') {
-//   console.log("Let's study JavaScript!");
-// } else {
-//   console.log('Please try again!');
-// }
-
-for (x = 0; x < topics.length ; x++){
-    console.log(topics[x]);
+function selectTopics(){
+    
+    if (randomTopic === 'HTML') {
+    console.log("Let's study HTML!");
+    } else if (randomTopic === 'CSS') {
+    console.log("Let's study CSS!");
+    } else if (randomTopic === 'Git') {
+    console.log("Let's study Git!");
+    } else if (randomTopic === 'JavaScript') {
+    console.log("Let's study JavaScript!");
+    } else {
+    console.log('Please try again!');
+    }
 }
 
+function listTopics(){
+    for (x = 0; x < topics.length ; x++){
+        console.log(topics[x]);
+    }
+}
+
+listTopics();
+console.log("Above are the Topics we learned in Pre-work");
+
+selectTopics();
+console.log("Above is the Topic we will be learning first");
